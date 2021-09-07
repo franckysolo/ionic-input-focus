d34d476b8e0dd04c2bf1b1acb48a3af844a52753
franckysolo <franckysolo@gmail.com>
2021-09-07 17:51:44 +0200

    Initial commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,25 +5,60 @@
         <ion-title>Blank</ion-title>
       </ion-toolbar>
     </ion-header>
-    
+
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
           <ion-title size="large">Blank</ion-title>
         </ion-toolbar>
       </ion-header>
-    
+
       <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+        <h2>Ion Input Focus issue verification</h2>
+        <ion-list>
+          <ion-item>
+            <ion-label position="floating">Username</ion-label>
+            <ion-input type="text" name="username" v-model="username" autocomplete="off" />
+          </ion-item>
+          <ion-item>
+            <ion-label position="floating">Email</ion-label>
+            <ion-input type="email" name="email" v-model="email" autocomplete="off" />
+          </ion-item>
+          <ion-item>
+            <ion-label position="floating">Password</ion-label>
+            <ion-input type="password" name="password" v-model="password" autocomplete="off" />
+          </ion-item>
+          <ion-item>
+            <ion-label position="floating">Phone</ion-label>
+            <ion-input type="tel" name="phone" v-model="phone" autocomplete="off" />
+          </ion-item>
+          <h3>Debug</h3>
+          <pre>
+            username  {{ username }}
+            email  {{ email }}
+            password  {{ password }}
+            phone  {{ phone }}
+          </pre>
+        </ion-list>
       </div>
     </ion-content>
   </ion-page>
 </template>
 
-<script lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
-import { defineComponent } from 'vue';
+<script>
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonList,
+  IonItem,
+  IonLabel,
+  IonInput
+} from '@ionic/vue';
+
+import { defineComponent, reactive, toRefs } from 'vue';
 
 export default defineComponent({
   name: 'Home',
@@ -32,7 +67,23 @@ export default defineComponent({
     IonHeader,
     IonPage,
     IonTitle,
-    IonToolbar
+    IonToolbar,
+    IonList,
+    IonItem,
+    IonLabel,
+    IonInput
+  },
+  setup () {
+    const form = reactive({
+      username: '',
+      email: '',
+      password: '',
+      phone: ''
+    })
+
+    return {
+      ...toRefs(form)
+    }
   }
 });
 </script>
@@ -40,7 +91,7 @@ export default defineComponent({
 <style scoped>
 #container {
   text-align: center;
-  
+
   position: absolute;
   left: 0;
   right: 0;
@@ -56,9 +107,9 @@ export default defineComponent({
 #container p {
   font-size: 16px;
   line-height: 22px;
-  
+
   color: #8c8c8c;
-  
+
   margin: 0;
 }
 
